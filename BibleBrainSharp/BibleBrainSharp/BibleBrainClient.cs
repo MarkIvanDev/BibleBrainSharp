f299d71b11df2171fca359c7d4988a24f47f1c4d
Mark Ivan Basto <markivan.developer@outlook.com>
2022-03-19 22:01:15 +0800

    add overload for custom HttpClient

--- BibleBrainSharp/BibleBrainSharp/BibleBrainClient.cs
+++ BibleBrainSharp/BibleBrainSharp/BibleBrainClient.cs
@@ -29,17 +29,16 @@ namespace BibleBrainSharp
     {
         private readonly HttpClient httpClient;
 
-        public BibleBrainClient(string apiKey)
+        public BibleBrainClient(string apiKey) : this(apiKey, new HttpClient())
         {
-            httpClient = new HttpClient
-            {
-                BaseAddress = new Uri(ApiEndpoints.Base),
-                DefaultRequestHeaders =
-                {
-                    { "v", "4" },
-                    { "key", apiKey }
-                }
-            };
+        }
+
+        public BibleBrainClient(string apiKey, HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            this.httpClient.BaseAddress = new Uri(ApiEndpoints.Base);
+            this.httpClient.DefaultRequestHeaders.Add("v", "4");
+            this.httpClient.DefaultRequestHeaders.Add("key", apiKey);
         }
 
         public void Dispose()
