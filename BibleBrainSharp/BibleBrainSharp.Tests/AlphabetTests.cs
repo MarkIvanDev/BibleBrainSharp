070495333e8d75a15fa8dfd3a41cee315f367492
Mark Ivan Basto <markivan.developer@outlook.com>
2023-02-08 16:12:24 +0800

    add tests

---
+++ BibleBrainSharp/BibleBrainSharp.Tests/AlphabetTests.cs
@@ -0,0 +1,20 @@
+namespace BibleBrainSharp.Tests
+{
+    public class AlphabetTests
+    {
+        
+        [Fact]
+        public async Task GetAlphabets()
+        {
+            var alphabets = await Client.ApiClient.GetAlphabets();
+            Assert.NotNull(alphabets);
+        }
+
+        [Fact]
+        public async Task GetAlphabet()
+        {
+            var alphabet = await Client.ApiClient.GetAlphabet("Latn");
+            Assert.NotNull(alphabet);
+        }
+    }
+}
---
+++ BibleBrainSharp/BibleBrainSharp.Tests/NumberTests.cs
@@ -0,0 +1,20 @@
+﻿namespace BibleBrainSharp.Tests
+{
+    public class NumberTests
+    {
+        [Fact]
+        public async Task GetNumbers()
+        {
+            var numbers = await Client.ApiClient.GetNumbers();
+            Assert.NotNull(numbers);
+        }
+
+        [Fact]
+        public async Task GetNumber()
+        {
+            var number = await Client.ApiClient.GetNumber("thai");
+            Assert.NotNull(number);
+        }
+
+    }
+}
